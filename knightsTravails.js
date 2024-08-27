06c624f20fa37ae93593e1945cce30ec161f3df8
Ndzi Nfor <ndzinfor@gmail.com>
2024-08-27 22:01:09 +0100

    check if end position is reached

--- knightsTravails.js
+++ knightsTravails.js
@@ -12,4 +12,16 @@ function knightMoves(start, end) {
     const visited = new Set();
     visited.add(`${start[0]},${start[1]}`);
 
+    while (queue.length > 0) {
+        const { position, path } = queue.shift();
+        const [x, y] = position;
+
+        // Check if we reached the end position
+        if (x === end[0] && y === end[1]) {
+            console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+            path.forEach(square => console.log(square));
+            return path; // Return the path taken to reach the end
+        }
+
+        
 }
